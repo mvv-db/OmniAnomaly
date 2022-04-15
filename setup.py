5fecd473e45ceb795ae17d276f4a3dc811009e0f
mvv-db <martin.vv@databricks.com>
2022-04-15 16:44:39 -0700

    updating setup.py

--- setup.py
+++ setup.py
@@ -4,10 +4,11 @@ with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
 setup(
-    name="omnianomaly",
+    name="omni_anomaly",
     version="0.0.1",
     python_requires=">=3.6",
     author="Some cool dude",
     long_description="Some cool project",
+    packages=["omni_anomaly"],
     install_requires=requirements,
 )
